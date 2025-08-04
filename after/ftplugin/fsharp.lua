200bd5f10253ef4b075e99aa53926227a4e72caa
dantan96 <danieltanios@gmail.com>
2025-08-04 23:33:15 +1000

    Fixed tab problem

---
+++ after/ftplugin/fsharp.lua
@@ -0,0 +1,9 @@
+-- Use spaces for indentation in F# (required by lightweight syntax)
+vim.opt_local.expandtab = true
+vim.opt_local.shiftwidth = 4
+vim.opt_local.softtabstop = 4
+vim.opt_local.tabstop = 4
+
+-- Optional: make tabs visible while editing
+-- vim.opt_local.list = true
+-- vim.opt_local.listchars:append({ tab = "» " })
