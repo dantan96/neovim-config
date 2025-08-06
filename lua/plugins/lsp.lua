6ffe02d5f36c0d2d123a81572a2505a1abfa7409
dantan96 <danieltanios@gmail.com>
2025-08-07 05:02:03 +1000

    fixed opt error

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,37 +4,36 @@ return {
     dependencies = {
       "saghen/blink.cmp",
     },
-    opts = {
-      -- capabilities for completion and other LSP features
-      capabilities = require("blink.cmp").get_lsp_capabilities(),
+    config = function()
+      local lspconfig = require("lspconfig")
+      local util = require("lspconfig.util")
+      local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-      -- per-server settings
-      servers = {
-        lua_ls = {},
-        basedpyright = {},
-        texlab = {},
+      -- Lua, Python and LaTeX servers
+      lspconfig.lua_ls.setup({ capabilities = capabilities })
+      lspconfig.basedpyright.setup({ capabilities = capabilities })
+      lspconfig.texlab.setup({ capabilities = capabilities })
 
-        fsautocomplete = {
-          cmd = { "fsautocomplete" },
-          init_options = { AutomaticWorkspaceInit = true },
-          root_dir = function(fname)
-            local util = require("lspconfig.util")
-            -- use vim.fs.dirname as a fallback on Neovim ≥0.10
-            return util.root_pattern("*.sln", "*.fsproj", ".git")(fname) or vim.fs.dirname(fname)
-          end,
-          filetypes = { "fsharp", "fs", "fsx", "fsi" },
-        },
-      },
-    },
-    -- config = function()
-    --   vim.api.nvim_create_autocmd("LspAttach", {
-    --     callback = function(args)
-    --       local client = vim.lsp.get_client_by_id(args.data.client_id)
-    --       if not client then
-    --         return
-    --       end
-    --     end,
-    --   })
-    -- end,
+      -- FsAutoComplete for F#
+      lspconfig.fsautocomplete.setup({
+        cmd = { "fsautocomplete" },
+        init_options = { AutomaticWorkspaceInit = true },
+        filetypes = { "fsharp", "fs", "fsx", "fsi" },
+        root_dir = function(fname)
+          -- Use lspconfig.util.root_pattern first, fall back to vim.fs.dirname
+          return util.root_pattern("*.sln", "*.fsproj", ".git")(fname) or vim.fs.dirname(fname)
+        end,
+        capabilities = capabilities,
+      })
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if not client then
+            return
+          end
+        end,
+      })
+    end,
   },
 }
