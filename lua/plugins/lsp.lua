b1e202948c7d83e25b62ea335c528ec5ecf756c4
dantan96 <danieltanios@gmail.com>
2025-08-07 13:16:46 +1000

    lua_ls not attaching. lmao. in any case, did some radical simplifications to lsp.lua bcause why not

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,30 +1,19 @@
+-- lua/plugins/lsp.lua
 return {
   {
     "neovim/nvim-lspconfig",
     dependencies = {
       "saghen/blink.cmp",
     },
+    opts = {
+      capabilities = require("blink.cmp").get_lsp_capabilities(),
+      servers = {
+        lua_ls = {},
+        basedpyright = {},
+        texlab = {},
+      },
+    },
     config = function()
-      local lspconfig = require("lspconfig")
-      local capabilities = require("blink.cmp").get_lsp_capabilities()
-
-      -- Lua, Python and LaTeX servers
-      lspconfig.lua_ls.setup({ capabilities = capabilities })
-      lspconfig.basedpyright.setup({ capabilities = capabilities })
-      lspconfig.texlab.setup({ capabilities = capabilities })
-
-      -- FsAutoComplete for F#
-      -- lspconfig.fsautocomplete.setup({
-      --   cmd = { "fsautocomplete" },
-      --   init_options = { AutomaticWorkspaceInit = true },
-      --   filetypes = { "fsharp", "fs", "fsx", "fsi" },
-      --   root_dir = function(fname)
-      --     -- Use lspconfig.util.root_pattern first, fall back to vim.fs.dirname
-      --     return util.root_pattern("*.sln", "*.fsproj", ".git")(fname) or vim.fs.dirname(fname)
-      --   end,
-      --   capabilities = capabilities,
-      -- })
-
       vim.api.nvim_create_autocmd("LspAttach", {
         callback = function(args)
           local client = vim.lsp.get_client_by_id(args.data.client_id)
